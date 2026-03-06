a8e1d875415a4aa3e05f2d1c469bd7262a92c1bb
Gaurav Singh <gauravsingh6436@gmial.com>
2026-03-06 20:30:46 +0530

    setup backend and frontend environment

---
+++ frontend/src/pages/ChatPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from "axios";
+import { useEffect,useState } from "react";
+const ChatPage = () => {
+    const [chats,setChats]=useState([]);
+    const fetchChats = async () => {
+        const data=await axios.get("/api/chats");
+        console.log(data);
+        setChats(data);
+    }
+    useEffect(() => {
+        fetchChats();
+    },[]);
+    
+    return <div>
+        {chats.map((chat)=> <div key={chat.id}>{chat.chatName}</div>)}
+    </div> 
+}
+export default ChatPage;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,5 @@
+import React from "react";
+const HomePage=()=>{
+    return <div> <h1>Home Page</h1></div>
+}
+export default HomePage;
